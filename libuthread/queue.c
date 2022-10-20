30b767d4424661ed32f6fb34a45ad087af7e5989
ansellum <jbvogt@ucdavis.edu>
2022-10-20 14:57:14 -0700

    Structured queue around linked list

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -1,45 +1,63 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "queue.h"
 
+typedef struct node
+{
+        void *data;     // holds an address to an unknown data type
+        struct node* next;
+}node;
+
 struct queue {
-	/* TODO Phase 1 */
+
+        int size;
+
+        node* first;
+        node* last;
 };
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+        queue_t queue = (queue_t)malloc(sizeof(queue_t));
+
+        queue->size = 0;
+        queue->first = (node*)malloc(sizeof(node));
+        queue->last = (node*)malloc(sizeof(node));
+        queue->first->next = queue->last;
+
+        return queue;
 }
 
 int queue_destroy(queue_t queue)
 {
-	/* TODO Phase 1 */
+        /* TODO Phase 1 */
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+        /* TODO Phase 1 */
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
-	/* TODO Phase 1 */
+        /* TODO Phase 1 */
 }
 
 int queue_delete(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+        /* TODO Phase 1 */
 }
 
 int queue_iterate(queue_t queue, queue_func_t func)
 {
-	/* TODO Phase 1 */
+        /* TODO Phase 1 */
 }
 
 int queue_length(queue_t queue)
 {
-	/* TODO Phase 1 */
+        /* TODO Phase 1 */
 }
 
