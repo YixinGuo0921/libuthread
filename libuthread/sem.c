e9a3474a66678fabd50069a5fc3b84d43a66aa34
ansellum <jbvogt@ucdavis.edu>
2022-10-29 15:56:06 -0700

    Changed from TABS to SPACES

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -13,69 +13,69 @@
 extern uthread_ctx_t* idle_ctx;
 
 struct semaphore { //sem_t is a pointer to a struct semaphore
-	queue_t waiting_room;
-	unsigned int resource;
+        queue_t waiting_room;
+        unsigned int resource;
 };
 
 struct uthread_tcb {
-	uthread_ctx_t* thread_ctx;
-	void* stack_ptr;
-	int state;
+        uthread_ctx_t* thread_ctx;
+        void* stack_ptr;
+        int state;
 };
 
 sem_t sem_create(size_t count)
 {
-	sem_t new_sem = malloc(sizeof(struct semaphore));
-	new_sem->waiting_room = queue_create();
+        sem_t new_sem = malloc(sizeof(struct semaphore));
+        new_sem->waiting_room = queue_create();
 
-	new_sem->resource = count;
-	return new_sem;
+        new_sem->resource = count;
+        return new_sem;
 }
 
 int sem_destroy(sem_t sem)
 {
-	free(sem);
-	return 0;
+        free(sem);
+        return 0;
 }
 
 int sem_down(sem_t sem)
 {
-	// Get caller thread
-	struct uthread_tcb* caller_tcb = uthread_current();
+        // Get caller thread
+        struct uthread_tcb* caller_tcb = uthread_current();
 
-	// If resources are available, take one and continue run
-	if (sem->resource != 0) {
-		sem->resource--;
-		return 0;
-	}
+        // If resources are available, take one and continue run
+        if (sem->resource != 0) {
+                sem->resource--;
+                return 0;
+        }
 
-	/* NO RESOURCES AVAILABLE */
+        /* NO RESOURCES AVAILABLE */
 
-	// Keep record of threads waiting for resource (FIFO)
-	queue_enqueue(sem->waiting_room, caller_tcb);
+        // Keep record of threads waiting for resource (FIFO)
+        queue_enqueue(sem->waiting_room, caller_tcb);
 
-	// block caller thread and go to next thread in thread_queue
-	caller_tcb->state = BLOCKED;
-	uthread_ctx_switch(caller_tcb->thread_ctx, idle_ctx);
+        // block caller thread and go to next thread in thread_queue
+        caller_tcb->state = BLOCKED;
+        uthread_ctx_switch(caller_tcb->thread_ctx, idle_ctx);
 
-	return -1;
+        return -1;
 }
 
 int sem_up(sem_t sem)
 {
-	struct uthread_tcb* blocked_tcb;
+        struct uthread_tcb* blocked_tcb;
 
-	// If no one in waiting room, continue thread
-	if (queue_length(sem->waiting_room) == 0)
-	{
-		sem->resource++;
-		return 0;
-	}
+        // If no one in waiting room, continue thread
+        if (queue_length(sem->waiting_room) == 0)
+        {
+                sem->resource++;
+                return 0;
+        }
 
-	// Otherwise, dequeue from waiting room and make thread available for queue again
-	queue_dequeue(sem->waiting_room, (void**)&blocked_tcb);
-	blocked_tcb->state = READY;
+        // Otherwise, dequeue from waiting room and make thread available for queue again
+        queue_dequeue(sem->waiting_room, (void**)&blocked_tcb);
+        blocked_tcb->state = READY;
 
-	return 0;
+        return 0;
 }
 
